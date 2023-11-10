affb25853ecbeb63ea94b5f49afe324db68726d5
igor-alves1 <ig_rodrigues@id.uff.br>
2023-11-10 13:43:08 -0300

    Enemies

--- game.py
+++ game.py
@@ -16,7 +16,18 @@ def game(screen, difficulty):
     bullets_list = []
     bullet_cooldown = 1
     bullet_timer = 0
+
+    col_inimigos = 6
+    lin_inimigos = 4
+    inimigos = [[], [], [], []]
+    inimigo_speed = 100
     
+    for i in range(lin_inimigos):
+        for j in range(col_inimigos):
+            new_sprite = Sprite("invader.png")
+            new_sprite.set_position(new_sprite.width+j*(3*new_sprite.width/2), new_sprite.height+i*(3*new_sprite.height/2))
+            inimigos[i].append(new_sprite)
+
     while True:
         tela_timer = screen.delta_time()
         bullet_timer += tela_timer
@@ -28,6 +39,20 @@ def game(screen, difficulty):
         for item in bullets_list:
             item.draw()
             item.move_y((-bullet_speed)*tela_timer)
+
+        for i in range(lin_inimigos):
+            for j in range(col_inimigos):
+                inimigos[i][j].move_x(inimigo_speed*tela_timer)
+                inimigos[i][j].draw()
+
+        if inimigos[0][-1].x >= screen.width-(inimigos[0][0].width/2) or inimigos[0][0].x <= inimigos[0][0].width/2:
+            inimigo_speed *= -1
+            for i in range(lin_inimigos):
+                for j in range(col_inimigos):
+                    inimigos[i][j].y += 10
+
+        if inimigos[-1][0].y >= screen.height - 3*player.height:
+            break
 
         if len(bullets_list) and bullets_list[0].y > screen.height:
             bullets_list.pop(0)
